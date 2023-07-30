d13c4bc888cda2c98f3f7f033742bf72513c24c0
nogi <Createc369.Re@gmail.com>
2023-07-30 12:57:45 +0900

    fix: データ定義の修正

---
+++ src/constants/data.js
@@ -0,0 +1,12 @@
+// 相違点：data ＝＞ INIT_TODO_LIST(定数名)
+
+export const INIT_TODO_LIST = [
+    {
+        id: 1,
+        content: "Todo1",
+    },
+    {
+        id: 2,
+        content: "Todo2",
+    },
+];
